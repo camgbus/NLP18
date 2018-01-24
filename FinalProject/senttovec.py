d17b73a8f749eedc6b2d105855722159559cd320
SalemBorhanYoussef <Salem.borhanyoussef@hotmail.de>
2018-01-24 14:25:21 +0100

    Create senttovec.py

---
+++ FinalProject/senttovec.py
@@ -0,0 +1,14 @@
+
+'''
+param class tokens: list of most representative tokens for a category
+[str1, str2, ...]
+result: vector representation
+'''
+def ...
+
+'''
+param v1: vector rep
+param v2: vector rep
+result: distane between vectors
+'''
+def
